11fb3a24a63013ca32e59d4d949ccdc289a3cd6b
mlbiam <marc@tremolo.io>
2021-07-23 12:28:06 -0400

    run delete

--- chapter9/delete_all_pods_except_gatekeeper.sh
+++ chapter9/delete_all_pods_except_gatekeeper.sh
@@ -3,5 +3,5 @@
 for each in $(kubectl get ns | tail -n +2 | awk '{print $1}' | grep -v gatekeeper-system);
 do
   echo "$each"
-  kubectl delete pods -n $each
+  kubectl delete pods -n "$each"
 done
