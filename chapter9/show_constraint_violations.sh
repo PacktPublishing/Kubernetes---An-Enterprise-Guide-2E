7c78b697a54d8396a52a208fedb383d458aa0a25
mlbiam <marc@tremolo.io>
2021-07-24 14:09:16 -0400

    add violation

--- chapter9/show_constraint_violations.sh
+++ chapter9/show_constraint_violations.sh
@@ -2,5 +2,5 @@
 
 for constraint in $(kubectl get crds | grep 'constraints.gatekeeper.sh' | awk '{print $1}');
 do      
-        echo $constraint
+        echo "$constraint $(kubectl get $constraint -o json | jq -r 'status.totalViolations')"
 done
