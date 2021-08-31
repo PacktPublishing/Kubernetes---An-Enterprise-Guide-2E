0e7b3bb015c62649972a79045c14947ba01ac587
mlbiam <marc@tremolo.io>
2021-08-31 15:16:02 -0400

    update

--- chapter13/authentication/deploy-auth.sh
+++ chapter13/authentication/deploy-auth.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
+export hostip="192-168-2-119"
 
-export jwks=$(curl --insecure $(curl --insecure https://k8sou.$hostip.nip.io/auth/idp/k8sIdp/.well-known/openid-configuration 2>/dev/null | jq -r '.jwks_uri') 2>/dev/null | jq -c)
+echo "getting oidc config"
+export oidc_config=$(curl --insecure https://k8sou.$hostip.nip.io/auth/idp/k8sIdp/.well-known/openid-configuration 2>/dev/null | jq -r '.jwks_uri')
+echo $oidc_config
+
+echo "getting jwks"
+export jwks=$(curl --insecure $oidc_config 2>/dev/null | jq -c)
 
 echo $hostip
 echo $jwks
