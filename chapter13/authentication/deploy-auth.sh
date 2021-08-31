5751b7a6af947a74832b1768c038e248da994595
mlbiam <marc@tremolo.io>
2021-08-31 15:28:43 -0400

    automate

--- chapter13/authentication/deploy-auth.sh
+++ chapter13/authentication/deploy-auth.sh
@@ -5,10 +5,11 @@ export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 
 echo "getting oidc config"
 export oidc_config=$(curl --insecure https://k8sou.$hostip.nip.io/auth/idp/k8sIdp/.well-known/openid-configuration 2>/dev/null | jq -r '.jwks_uri')
-echo $oidc_config
+
 
 echo "getting jwks"
 export jwks=$(curl --insecure $oidc_config 2>/dev/null | jq -c '.')
 
-echo $hostip
-echo $jwks
+sed "s/IPADDR/$hostip/g" < ./service-auth.yaml | "s/JWKS_FROM_SERVER/$jwks/g" > /tmp/service-auth.yaml
+
+kubectl create -f /tmp/service-auth.yaml
