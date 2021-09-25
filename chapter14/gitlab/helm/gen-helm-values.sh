976cd680c28a02c19fdd57e2155686826d65e1c7
mlbiam <marc@tremolo.io>
2021-09-24 21:50:38 -0400

    no mutations for gitlab, wildcard cert

--- chapter14/gitlab/helm/gen-helm-values.sh
+++ chapter14/gitlab/helm/gen-helm-values.sh
@@ -3,3 +3,5 @@
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 
 sed "s/IPADDR/$hostip/g" < gitlab-values.yaml > /tmp/gitlab-values.yaml
+
+sed "s/IPADDR/$hostip/g" < gitlab-wildcard-tls.yaml | kubectl create -f - 
