d18f5e3fcc6b9c1f9a0606a70d6aa413e5e2c344
mlbiam <marc@tremolo.io>
2021-09-24 20:57:00 -0400

    fix script

--- chapter14/gitlab/helm/gen-helm-values.sh
+++ chapter14/gitlab/helm/gen-helm-values.sh
@@ -2,4 +2,4 @@
 
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 
-sed "s/IPADDR/$hostip/g" < gitlab.values > /tmp/gitlab-values.yaml
+sed "s/IPADDR/$hostip/g" < gitlab-values.yaml > /tmp/gitlab-values.yaml
