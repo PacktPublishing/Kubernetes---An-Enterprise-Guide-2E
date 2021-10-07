605bd87755255f6acf7950585336a72b2a5d5010
mlbiam <marc@tremolo.io>
2021-10-07 13:50:40 -0400

    add app name

--- chapter14/sample-repo/python-hello-operations/deployToGit.sh
+++ chapter14/sample-repo/python-hello-operations/deployToGit.sh
@@ -3,10 +3,12 @@
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 
 export destdir=$1
+export appname=$2
+
 
 mkdir -p $destdir/src/deployments
 
-sed "s/IPADDR/$hostip/g" < src/deployments/hello-python.yaml > $destdir/src/deployments/hello-python.yaml
+sed "s/IPADDR/$hostip/g" < src/deployments/hello-python.yaml | sed "s/APPNAME/$appname/g" > $destdir/src/deployments/hello-python.yaml
 
 cp README.md $destdir/
 
