6435e5977b3c6780e047558ea229b19b8b0df0c3
Scott Surovich <60396639+ssurovich@users.noreply.github.com>
2021-11-01 13:47:31 -0400

    Update create-gw-vs.sh

--- chapter12/example-app/create-gw-vs.sh
+++ chapter12/example-app/create-gw-vs.sh
@@ -25,10 +25,7 @@ echo -e "***********************************************************************
 envsubst < vs.yaml | kubectl apply -f - --namespace demo
 
 tput setaf 5
-echo -e "\n\nThe Gateway and VirtualService been ceated"
-echo -e "\n\nDue to the networking model of running KinD, we cannot directly hit the VirtualService, so we have created a NGINX"
-echo -e "that will forward the requests to the istio-ingressgateway so you can test the application, you can open the UI"
-echo -e "using http://demo.$hostip.nip.io/"
+echo -e "\n \nThe Boutique application can be accessed using using http://demo.$hostip.nip.io/"
 
 echo -e "\n\n"
 tput setaf 9
