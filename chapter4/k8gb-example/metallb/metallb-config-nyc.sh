8957f4495c5e42c27d194131b348f485ee379e89
robotica72 <surovich@gmail.com>
2021-11-07 17:23:00 -0500

    Update K8GB scripts

---
+++ chapter4/k8gb-example/metallb/metallb-config-nyc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+clear
+
+tput setaf 5
+echo -e "\n*******************************************************************************************************************"
+echo -e "Installing MetalLB for the NYC Cluster"
+echo -e "*******************************************************************************************************************"
+tput setaf 2
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+tput setaf 5
+echo -e "\n*******************************************************************************************************************"
+echo -e "Configuring MetalLB IP Pool for NYC Cluster"
+echo -e "*******************************************************************************************************************"
+tput setaf 2
+kubectl apply -f metallb-config-nyc.yaml
+
+tput setaf 3
+echo -e "\n*******************************************************************************************************************"
+echo -e "MetalLB installation complete"
+echo -e "*******************************************************************************************************************\n"
+tput setaf 2
+
+kubectl get pods -n metallb-system
+
+echo -e "\n\n"
