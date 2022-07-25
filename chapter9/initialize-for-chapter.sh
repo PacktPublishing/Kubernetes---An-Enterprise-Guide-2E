7a1f3254bbc5dfd2316471dc92394b1bb3fe8614
Marc Boorshtein <marc@tremolo.io>
2022-07-25 11:42:49 -0400

    get gatekeeper working from chapter 8

--- chapter9/initialize-for-chapter.sh
+++ chapter9/initialize-for-chapter.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# first, deploy openunison
+
+cd ../chapter6/openunison/
+./deploy_openunison_imp.sh
+
+# deploy gatekeeper
+kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/release-3.5/deploy/gatekeeper.yaml
+kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/release-3.5/deploy/experimental/gatekeeper-mutation.yaml
