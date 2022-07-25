ffac3211de84883b65f7d14bd7765243185c167f
mlbiam <marc@tremolo.io>
2022-07-25 13:18:16 -0400

    make run from root dir

--- chapter9/initialize-for-chapter.sh
+++ chapter9/initialize-for-chapter.sh
@@ -2,8 +2,9 @@
 
 # first, deploy openunison
 
-cd ../chapter6/openunison/
+cd chapter6/openunison/
 ./deploy_openunison_imp.sh
+cd ../../
 
 # deploy gatekeeper
 kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/release-3.5/deploy/gatekeeper.yaml
